939e2b2dce09b26554f971f5751921f5fdf16d28
JefersonSopanR <jeferson23sopan@gmail.com>
2025-12-09 12:35:28 +0100

    histogram: done

--- histogram.py
+++ histogram.py
@@ -1,6 +1,7 @@
 from DSLR.core import getNumPyArray
 import numpy
 from DSLR.describe_ import stdDev_, var_
+import matplotlib.pyplot as plt
 
 def find_best_col(data):
     houses = numpy.unique(data[:, 1])
@@ -37,9 +38,37 @@ def find_best_col(data):
 
 if __name__ == '__main__':
     data = getNumPyArray('dataset_train.csv')
+    headers = data[0]
     data = data[1:, :]
     indices = data[:, 1].argsort()
     data = data[indices]
 
     best_col = find_best_col(data)
-    print(data[:, best_col])
+    print(best_col)
+    houses = numpy.unique(data[:, 1])
+    color_houses = {
+        'Gryffindor': 'red',
+        'Hufflepuff': 'yellow',
+        'Ravenclaw': 'blue',
+        'Slytherin': 'green'
+    }
+    
+    # Plot histogram for each house
+    # this creates a blank canvas figsize(x, y)
+    plt.figure(figsize=(10, 6))
+
+    for house in houses:
+        house_data = data[data[:, 1] == house][:, best_col]
+        house_data = numpy.array(house_data, dtype=float)
+        house_data = house_data[~numpy.isnan(house_data)]
+        # this Draws the histogram bars:
+        plt.hist(house_data, alpha=0.5, label=house, bins=20)
+        # alpha=0.5 - Make bars 50% transparent (so overlapping bars are visible)
+        # label=house - Name for legend -> example ('Gryffindor')
+        # bins=20 - Divide score range into 20 groups (blocks)
+    
+    plt.xlabel('Score')
+    plt.ylabel('Frequency')
+    plt.title(f'Score Distribution - {headers[best_col]}')
+    plt.legend()
+    plt.show()
